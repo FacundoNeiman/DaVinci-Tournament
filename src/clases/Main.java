1c04af82fb51f7d6fe13988dfceffe4ebeb9fe34
escuela@davinci.edu.ar <escuela@davinci.edu.ar>
2022-10-24 12:34:42 -0300

    feat: add players

--- src/clases/Main.java
+++ src/clases/Main.java
@@ -13,8 +13,16 @@ public class Main {
         Player profeSergio = new Player("Sergio Medina", "Arquero", 0, 0);
         profesoresPOO.addPlayer(profeSergio);
         profesoresPOO.addPlayer(new Player("Lautaro 'Perk' Chiappero" ,"Lateral Derecho", 1,5));
+        profesoresPOO.addPlayer(new Player("Victor 'Emperador' Golemba", "Volante Central", 0 ,2));
+        profesoresPOO.addPlayer(new Player("Juan Carloni", "Lateral Izquierdo", 1, 2));
+        profesoresPOO.addPlayer(new Player("Elda Montoto", "Delantero",10,20));
 
         Team estudiantesFC = new Team("Estudiantes DaVinci");
+        estudiantesFC.addPlayer(new Player("Facundo Neiman", "Delantero", 8, 4));
+        estudiantesFC.addPlayer(new Player("Lucas Emens", "Defensor", 1, 8));
+        estudiantesFC.addPlayer(new Player("Kevin Colina", "Lateral Izquierdo", 0, 8));
+        estudiantesFC.addPlayer(new Player("Facundo Preiss", "Defensor", 0, 8));
+        estudiantesFC.addPlayer(new Player("Sebastian 'El Innombrable'", "Aguatero Suplente", 0, 0));
 
         Date fechaEstimadaParcial = new Date(2022, 8, 29);
         Match primerPartido = new Match("Open Gallo", fechaEstimadaParcial, estudiantesFC, profesoresPOO);
